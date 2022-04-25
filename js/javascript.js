964876f882c5b0821d161e297b56f22b85a49919
machadomrm <85651380+machadomrm@users.noreply.github.com>
2022-04-25 09:31:21 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -6,7 +6,6 @@ function addNewInputChildren(e) {
     e.preventDefault();
 
     let count = childrenContent.children.length;
-    // let comboEscolaridade = document.getElementById("CboEscolaridade");
 
     const section = document.createElement("section");
 
@@ -52,76 +51,6 @@ function addNewInputChildren(e) {
     DivEscolaridade.appendChild(LabelEscolaridade);
     DivEscolaridade.appendChild(SelectEscolaridade);
 
-    // for (i = 0; i < comboEscolaridade.length; i = i + 1) {
-    //     console.log(comboEscolaridade.options[i]);
-    // }
-
-    // <
-    // input type = "button"
-    // id = "btnCarregar"
-    // value = "Carregar combobox" / > ;
-
-    // document.getElementById("btnCarregar").onclick = function() {
-    //     var comboEscolaridade = document.getElementById("CboEscolaridade");
-
-    //     var opt0 = document.createElement("option");
-    //     opt0.value = "";
-    //     opt0.text = "Selecione";
-    //     comboEscolaridade.add(opt0, comboEscolaridade.options[0]);
-
-    //     var opt1 = document.createElement("option");
-    //     opt1.value = "2GC";
-    //     opt1.text = "Segundo Grau Completo";
-    //     comboEscolaridade.add(opt1, comboEscolaridade.options[1]);
-
-    //     var opt2 = document.createElement("option");
-    //     opt2.value = "2GI";
-    //     opt2.text = "Segundo Grau Incompleto";
-    //     comboEscolaridade.add(opt2, comboEscolaridade.options[2]);
-
-    //     var opt3 = document.createElement("option");
-    //     opt3.value = "CTC";
-    //     opt3.text = "Curso Técnico Completo";
-    //     comboEscolaridade.add(opt3, comboEscolaridade.options[3]);
-
-    //     var opt4 = document.createElement("option");
-    //     opt4.value = "CTI";
-    //     opt4.text = "Curso Técnico Incompleto";
-    //     comboEscolaridade.add(opt4, comboEscolaridade.options[4]);
-
-    //     var opt5 = document.createElement("option");
-    //     opt5.value = "NSC";
-    //     opt5.text = "Nível Superior Completo";
-    //     comboEscolaridade.add(opt5, comboEscolaridade.options[5]);
-
-    //     var opt6 = document.createElement("option");
-    //     opt6.value = "NSI";
-    //     opt6.text = "Nível Superior Incompleto";
-    //     comboEscolaridade.add(opt6, comboEscolaridade.options[6]);
-
-    //     var opt7 = document.createElement("option");
-    //     opt7.value = "PGC";
-    //     opt7.text = "Pós Graduação Completo";
-    //     comboEscolaridade.add(opt7, comboEscolaridade.options[7]);
-
-    //     var opt8 = document.createElement("option");
-    //     opt8.value = "PGI";
-    //     opt8.text = "Pós Graduação Incompleto";
-    //     comboEscolaridade.add(opt8, comboEscolaridade.options[8]);
-    // };
-    ///////////////////////////////////////////////////
-    // function atualizouSelect() {
-    //     let select = document.querySelector("#cboDependente");
-    //     let optionValue = select.option[select.selectedIndex];
-
-    //     let value = optionValue.value;
-    //     let text = optionValue.text;
-
-    //     console.log(value, text);
-    // }
-
-    // atualizouSelect();
-
     // dependente
     const DivDependente = document.createElement("div");
     DivDependente.className = "col-md-4";
@@ -130,7 +59,7 @@ function addNewInputChildren(e) {
     LabelDependente.setAttribute("for", "dependente" + count);
     LabelDependente.innerHTML = "Dependente";
     const SelectDependente = document.createElement("select");
-    SelectDependente.setAttribute("select", "name");
+    SelectDependente.setAttribute("select", "text");
     SelectDependente.setAttribute("class", "form-control");
     SelectDependente.setAttribute("id", "dependente" + count);
     SelectDependente.setAttribute("name", `dependente[${count}]`);
@@ -160,6 +89,13 @@ function addNewInputChildren(e) {
     section.style.cssText =
         "display:flex; flex-direction: row; flex-wrap: wrap; width:100%;";
     childrenContent.appendChild(section);
+}
+
+function apagaForm() {
+    document.getElementById("InputNomeFilho").reset();
+    document.getElementById("SelectEscolaridade").reset();
+    document.getElementById("SelectDependente").reset();
+    document.getElementById("SelectFir").reset();
 }
 
 add.addEventListener("click", addNewInputChildren);
